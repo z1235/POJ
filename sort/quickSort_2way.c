89a07c993177a25f23c88be56cef705940c1e87f
= <=>
2017-07-31 23:08:56 +0800

    add quick sort 2 ways

--- sort/quickSort_2way.c
+++ sort/quickSort_2way.c
@@ -1,47 +1,53 @@
-// 二路快速排序
 #include <stdio.h>
 #include <stdlib.h>
+#include "sortHelper.h"
+#include "quickSort.h"
 
-void swap(int *a, int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void _partiction(int *arr, int l, int r)
+int _partiction_2ways(int *arr, int l, int r)
 {
     int e = 0;
     int i = 0, j = 0;
     swap(&arr[l], &arr[rand() % (r - l + 1) + l]);
     e = arr[l];
 
-    // arr[l + 1, p] < e < arr[p + 1, r]
-    i = l;
-    j = r + 1;
-    while (i < j) {
-
+    i = l + 1;
+    j = r;
+    while (1) {
+        while (i <= r && arr[i] < e) i++;
+        while (j >= l + 1 && arr[j] > e) j--;
+        if (i > j) break;
+        swap(&arr[i], &arr[j]);
+        i++;
+        j--;
     }
+    swap(&arr[l], &arr[j]);
+    return j;
 }
 
-void _quickSort(int *arr, int l, int r)
+void _quicksort_2ways(int *arr, int l, int r)
 {
     int p = 0;
     if (l >= r)
         return;
-    p = _partiction(arr, l, r);
-    _quickSort(arr, l, p - 1);
-    _quickSort(arr, p + 1, r);
+    p = _partiction_2ways(arr, l, r);
+    _quicksort_2ways(arr, l, p - 1);
+    _quicksort_2ways(arr, p + 1, r);
 }
 
-void quickSort(int *arr, int n)
+void quicksort_2ways(int *arr, int n)
 {
     srand(time(NULL));
-    _quickSort(arr, 0, n - 1);
+    _quicksort_2ways(arr, 0, n - 1);
 }
 
 int main()
 {
-
+    int n = 100000;
+    int *arr = generateRandom(n, 1, 3);
+    int *arr1 = copyTestCase(arr, n);
+    testSortFunction("quicksort_2ways", quicksort_2ways, arr, n);
+    testSortFunction("quicksort normal", quickSort, arr1, n);
+    free(arr);
+    free(arr1);
     return 0;
 }
