5896cb4b67f19876a3249c9698629d1f0ad86211
= hui <= 799488441@qq.com>
2017-08-03 17:04:12 +0800

    2017-9-1-pm

---
+++ queue/queue.c
@@ -0,0 +1,71 @@
+// 环形队列的相关算法
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef int ElemType;
+typedef struct {
+    ElemType *data;
+    int capacity;
+    int front, rear; // 队首 队尾指针
+} Queue;
+
+Queue * initQueue(int capacity)
+{
+    Queue *q = (Queue *)malloc(sizeof(Queue));
+    q->data = (ElemType *)malloc(sizeof(ElemType) * (capacity));
+    q->capacity = capacity;
+    q->front = q->rear = 0;
+    return q;
+}
+
+void deleteQueue(Queue *q)
+{
+    if (!q) return;
+    if (q->data) {
+        free(q->data);
+        q->data = NULL;
+    }
+    free(q);
+    q = NULL;
+}
+
+int isEmpty(Queue *q)
+{
+    return q->rear == q->front;
+}
+
+void pushQueue(Queue *q, ElemType e)
+{
+    assert((q->rear + 1) % (q->capacity) != q->front);
+    q->rear = (q->rear + 1) % (q->capacity);
+    (q->data)[q->rear] = e;
+}
+
+void getFront(Queue *q, ElemType *e)
+{
+    assert(q->front != q->rear);
+    *e =  (q->data)[q->front + 1];
+}
+
+void popQueue(Queue *q)
+{
+    assert(q->front != q->rear);
+    q->front = (q->front + 1) % (q->capacity);
+}
+
+int main()
+{
+    Queue *q = initQueue(100);
+    ElemType e;
+    int i;
+    for (i = 0; i < 90; ++i)
+        pushQueue(q, i);
+    while (!isEmpty(q)) {
+        getFront(q, &e);
+        printf("%d ", e);
+        popQueue(q);
+    }
+    putchar('\n');
+    return 0;
+}
